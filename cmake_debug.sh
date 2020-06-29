2bf9638a09cb719904188b4d58a54893e7ebb93f
satanson <ranpanf@gmail.com>
2020-06-30 00:33:21 +0800

    fix cmake_debug.sh

--- cmake_debug.sh
+++ cmake_debug.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 set -e -o pipefail
-cmake -H. -Bdebug-build  -DCMAKE_BUILD_TYPE=Debug -D CMAKE_CXX_FLAGS_DEBUG="-O0 -g"
+basedir=$(cd $(dirname $(readlink -f ${BASH_SOURCE:-$0}));pwd)
+cd ${basedir}
+
+builddir=build_debug
+[ -d ${builddir} ] && rm -fr ${builddir}
+cmake -H. -B${builddir}  -DCMAKE_BUILD_TYPE=Debug -D CMAKE_CXX_FLAGS_DEBUG="-O0 -g"
+cd ${builddir}
+make
