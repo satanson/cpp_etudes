8271885e776f09001e305110e74d4783fea214ed
satanson <ranpanf@gmail.com>
2020-10-20 15:50:05 +0800

    merge clang and gcc CMakeLists.txt

---
+++ clang.sh
@@ -0,0 +1,1 @@
+env CC=clang CXX=clang++ cmake -G Ninja -H. -Bbuild_ninja_clang_sse -DCMAKE_BUILD_TYPE=Release
---
+++ gcc.sh
@@ -0,0 +1,1 @@
+cmake -G Ninja -H. -Bbuild_ninja_gcc_sse -DLLVM_ENABLE=off -DCMAKE_BUILD_TYPE=Release
