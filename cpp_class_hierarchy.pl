00aa96a78a596aee771464cb7aeb555ae937e2cd
satanson <ranpanf@gmail.com>
2020-11-21 02:22:18 +0800

    format pl script

--- cpp_class_hierarchy.pl
+++ cpp_class_hierarchy.pl
@@ -3,175 +3,197 @@ use warnings;
 use strict;
 use Data::Dumper;
 
-sub ensure_ag_installed(){
-  my ($ag_path)=map{chomp;$_} qx(which ag 2>/dev/null);
-  if (!defined($ag_path) || ( ! -e $ag_path)) {
-    printf STDERR "ag is missing, please install ag at first\n";
-    exit 1;
-  }
+sub ensure_ag_installed() {
+    my ($ag_path) = map {chomp;
+        $_} qx(which ag 2>/dev/null);
+    if (!defined($ag_path) || (!-e $ag_path)) {
+        printf STDERR "ag is missing, please install ag at first\n";
+        exit 1;
+    }
 }
 
 ensure_ag_installed;
 
-sub merge_lines(\@){
-  my @lines = @{+shift};
-  my @three_parts = map{/^(\S+):(\d+):(.*)$/; [$1,$2,$3]} @lines;
-  my ($prev_file, $prev_lineno, $prev_line) = @{$three_parts[0]};
-  my $prev_lineno_adjacent=$prev_lineno;
-  my @result=();
-  for(my $i=1; $i < scalar(@three_parts); ++$i){
-    my ($file, $lineno, $line) = @{$three_parts[$i]};
-    if (($file eq $prev_file) && ($prev_lineno_adjacent + 1 == $lineno)){
-      $prev_line  = $prev_line . $line;
-      $prev_lineno_adjacent += 1;
-    } else {
-      push @result, join(":", $prev_file, $prev_lineno, $prev_line);
-      ($prev_file, $prev_lineno, $prev_line)=($file, $lineno, $line);
-      $prev_lineno_adjacent = $prev_lineno;
+sub merge_lines(\@) {
+    my @lines = @{+shift};
+    my @three_parts = map {/^(\S+):(\d+):(.*)$/;
+        [ $1, $2, $3 ]} @lines;
+    my ($prev_file, $prev_lineno, $prev_line) = @{$three_parts[0]};
+    my $prev_lineno_adjacent = $prev_lineno;
+    my @result = ();
+    for (my $i = 1; $i < scalar(@three_parts); ++$i) {
+        my ($file, $lineno, $line) = @{$three_parts[$i]};
+        if (($file eq $prev_file) && ($prev_lineno_adjacent + 1 == $lineno)) {
+            $prev_line = $prev_line . $line;
+            $prev_lineno_adjacent += 1;
+        }
+        else {
+            push @result, join(":", $prev_file, $prev_lineno, $prev_line);
+            ($prev_file, $prev_lineno, $prev_line) = ($file, $lineno, $line);
+            $prev_lineno_adjacent = $prev_lineno;
+        }
     }
-  }
-  push @result, join(":", $prev_file, $prev_lineno, $prev_line);
-  return @result;
+    push @result, join(":", $prev_file, $prev_lineno, $prev_line);
+    return @result;
 }
 
-sub all_sub_classes(){
-  my $attr_re="\\[\\[[^\\[\\]]+\\]\\]";
-  my $access_specifier_re="final|private|public|protected";
-  my $template_arguments_re="<([^<>]*(?:<(?1)>|[^<>])[^<>]*)?>";
-  my $cls_re="^\\s*(template\\s*$template_arguments_re)?\\s*\\b(class|struct)\\b\\s*([a-zA-Z]\\w+[a-zA-Z])\\s*[^{};*()=]*?{";
-  my $cls_filter_re="^(\\S+)\\s*:\\s*(?:class|struct)\\s+\\w+(\\s+:\\s+(\\s*[:\\w]+\\s*,\\s*)*[:\\w]+)?s*\$";
+sub all_sub_classes() {
+    my $attr_re = "\\[\\[[^\\[\\]]+\\]\\]";
+    my $access_specifier_re = "final|private|public|protected";
+    my $template_arguments_re = "<([^<>]*(?:<(?1)>|[^<>])[^<>]*)?>";
+    my $cls_re = "^\\s*(template\\s*$template_arguments_re)?\\s*\\b(class|struct)\\b\\s*([a-zA-Z]\\w+[a-zA-Z])\\s*[^{};*()=]*?{";
+    my $cls_filter_re = "^(\\S+)\\s*:\\s*(?:class|struct)\\s+\\w+(\\s+:\\s+(\\s*[:\\w]+\\s*,\\s*)*[:\\w]+)?s*\$";
 
-  my $class0_re="(?:class|struct)\\s+(\\w+)";
-  my $class1_re="(?:class|struct)\\s+(\\w+)\\s*:\\s*([:\\w]+)";
-  my $class2_re="(?:class|struct)\\s+(\\w+)\\s*:(?:\\s*[:\\w]+\\s*,){1}\\s*([:\\w]+)";
-  my $class3_re="(?:class|struct)\\s+(\\w+)\\s*:(?:\\s*[:\\w]+\\s*,){2}\\s*([:\\w]+)";
-  my $class4_re="(?:class|struct)\\s+(\\w+)\\s*:(?:\\s*[:\\w]+\\s*,){3}\\s*([:\\w]+)";
-  my $class5_re="(?:class|struct)\\s+(\\w+)\\s*:(?:\\s*[:\\w]+\\s*,){4}\\s*([:\\w]+)";
-  my $class6_re="(?:class|struct)\\s+(\\w+)\\s*:(?:\\s*[:\\w]+\\s*,){5}\\s*([:\\w]+)";
+    my $class0_re = "(?:class|struct)\\s+(\\w+)";
+    my $class1_re = "(?:class|struct)\\s+(\\w+)\\s*:\\s*([:\\w]+)";
+    my $class2_re = "(?:class|struct)\\s+(\\w+)\\s*:(?:\\s*[:\\w]+\\s*,){1}\\s*([:\\w]+)";
+    my $class3_re = "(?:class|struct)\\s+(\\w+)\\s*:(?:\\s*[:\\w]+\\s*,){2}\\s*([:\\w]+)";
+    my $class4_re = "(?:class|struct)\\s+(\\w+)\\s*:(?:\\s*[:\\w]+\\s*,){3}\\s*([:\\w]+)";
+    my $class5_re = "(?:class|struct)\\s+(\\w+)\\s*:(?:\\s*[:\\w]+\\s*,){4}\\s*([:\\w]+)";
+    my $class6_re = "(?:class|struct)\\s+(\\w+)\\s*:(?:\\s*[:\\w]+\\s*,){5}\\s*([:\\w]+)";
 
-  my $cache_file=".cpp_class_hierarchy.list";
-  my @matches=();
-  if ((-f $cache_file) && (((-M $cache_file)*3600*24) < 600)) {
-    @matches=map {chomp; $_} qx(cat $cache_file);
-    qx(touch $cache_file);
-  } else {
-    @matches=map {chomp;$_} qx(ag -G '\.(c|cc|cpp|C|h|hh|hpp|H)\$' '$cls_re');
-    @matches=merge_lines @matches;
-    @matches=map {s/$attr_re//g; $_} @matches;
-    @matches=map {s/$access_specifier_re//g; $_} @matches;
-    @matches=map {s/$template_arguments_re//g; $_} @matches;
-    @matches=map {s/$template_arguments_re//g; $_} @matches;
-    @matches=map {s/$template_arguments_re//g; $_} @matches;
-    @matches=map {s/$template_arguments_re//g; $_} @matches;
-    @matches=map {s/template//g; $_} @matches;
-    @matches=map {s/^(.+?)\s*{/$1/g; $_} @matches;
-    @matches=map {s/\s+/ /g; $_} @matches;
-    @matches=grep {/$cls_filter_re/} @matches;
-    open my $cache_file_handle, "+>", $cache_file or die "$!";
-    print $cache_file_handle join("\n", @matches);
-    close($cache_file_handle);
-    #qx(echo -n > $cache_file);
-    #foreach my $line (@matches) {
-    #  #print qq(echo "$line" >>$cache_file), "\n";
-    #  qx(echo "$line" >>$cache_file);
-    #}
-  }
-
-  #print Dumper(\@matches);
-
-  my %table=map{$_->[1]=>$_} grep {defined($_)} map{ if (/^(\S+)\s*:\s*$class0_re/){[$1,$2]}else{print "undefined:$_\n"; undef}} @matches;
-  #print Dumper(\%table);
-
-  my $tree={};
-  my @class_re_list=($class1_re, $class2_re, $class3_re, $class4_re, $class5_re, $class6_re);
-
-  @matches=map {$_->[1] } sort{$a->[0] cmp $b->[0]} map {/^(\S+)\s*:\s*$class0_re/; [$2, $_]} @matches;
-  for my $re (@class_re_list){
-    my @parent_matches=grep {defined($_)} map{ if (/^(\S+)\s*:\s*$re\s*$/){[$1,$2,$3]}else{undef}} @matches;
-    if (scalar(@parent_matches)==0){
-      last;
+    my $cache_file = ".cpp_class_hierarchy.list";
+    my @matches = ();
+    if ((-f $cache_file) && (((-M $cache_file) * 3600 * 24) < 600)) {
+        @matches = map {chomp;
+            $_} qx(cat $cache_file);
+        qx(touch $cache_file);
     }
-    for my $e (@parent_matches) {
-      my ($fileline, $child, $parent)=@$e;
-      if (!defined($tree->{$parent})){
-        $tree->{$parent}=[];
-      }
-      push @{$tree->{$parent}}, $e;
+    else {
+        @matches = map {chomp;
+            $_} qx(ag -G '\.(c|cc|cpp|C|h|hh|hpp|H)\$' '$cls_re');
+        @matches = merge_lines @matches;
+        @matches = map {s/$attr_re//g;
+            $_} @matches;
+        @matches = map {s/$access_specifier_re//g;
+            $_} @matches;
+        @matches = map {s/$template_arguments_re//g;
+            $_} @matches;
+        @matches = map {s/$template_arguments_re//g;
+            $_} @matches;
+        @matches = map {s/$template_arguments_re//g;
+            $_} @matches;
+        @matches = map {s/$template_arguments_re//g;
+            $_} @matches;
+        @matches = map {s/template//g;
+            $_} @matches;
+        @matches = map {s/^(.+?)\s*{/$1/g;
+            $_} @matches;
+        @matches = map {s/\s+/ /g;
+            $_} @matches;
+        @matches = grep {/$cls_filter_re/} @matches;
+        open my $cache_file_handle, "+>", $cache_file or die "$!";
+        print $cache_file_handle join("\n", @matches);
+        close($cache_file_handle);
+        #qx(echo -n > $cache_file);
+        #foreach my $line (@matches) {
+        #  #print qq(echo "$line" >>$cache_file), "\n";
+        #  qx(echo "$line" >>$cache_file);
+        #}
     }
-  } 
-  return $tree, \%table;
+
+    #print Dumper(\@matches);
+
+    my %table = map {$_->[1] => $_} grep {defined($_)} map {if (/^(\S+)\s*:\s*$class0_re/) {[ $1, $2 ]}
+    else {
+        print "undefined:$_\n";
+        undef
+    }} @matches;
+    #print Dumper(\%table);
+
+    my $tree = {};
+    my @class_re_list = ($class1_re, $class2_re, $class3_re, $class4_re, $class5_re, $class6_re);
+
+    @matches = map {$_->[1]} sort {$a->[0] cmp $b->[0]} map {/^(\S+)\s*:\s*$class0_re/;
+        [ $2, $_ ]} @matches;
+    for my $re (@class_re_list) {
+        my @parent_matches = grep {defined($_)} map {if (/^(\S+)\s*:\s*$re\s*$/) {[ $1, $2, $3 ]}
+        else {undef}} @matches;
+        if (scalar(@parent_matches) == 0) {
+            last;
+        }
+        for my $e (@parent_matches) {
+            my ($fileline, $child, $parent) = @$e;
+            if (!defined($tree->{$parent})) {
+                $tree->{$parent} = [];
+            }
+            push @{$tree->{$parent}}, $e;
+        }
+    }
+    return $tree, \%table;
 }
 
-my $cls=shift || die "missing class name";
-my $verbose=shift;
-my ($tree,$table)=all_sub_classes();
+my $cls = shift || die "missing class name";
+my $verbose = shift;
+my ($tree, $table) = all_sub_classes();
 
-sub sub_class($){
-  my ($cls)=@_;
-  my $root={name=>$cls,child=>[]};
-  if (! exists $tree->{$cls}){
+sub sub_class($) {
+    my ($cls) = @_;
+    my $root = { name => $cls, child => [] };
+    if (!exists $tree->{$cls}) {
+        return $root;
+    }
+
+    my $child = $tree->{$cls};
+
+    foreach my $chd (@$child) {
+        push @{$root->{child}}, &sub_class($chd->[1]);
+    }
     return $root;
-  }
-
-  my $child=$tree->{$cls};
-
-  foreach my $chd  (@$child) {
-    push @{$root->{child}}, &sub_class($chd->[1]);
-  }
-  return $root;
 }
 
-sub fuzzy_sub_class($){
-  my ($cls_pattern)=@_;
-  my $root={name=>$cls_pattern};
-  my @names = grep {/$cls_pattern/} (keys %$table);
-  #print Dumper(\@names);
-  $root->{child}=[map {&sub_class($_)} @names]; 
-  return $root;
+sub fuzzy_sub_class($) {
+    my ($cls_pattern) = @_;
+    my $root = { name => $cls_pattern };
+    my @names = grep {/$cls_pattern/} (keys %$table);
+    #print Dumper(\@names);
+    $root->{child} = [ map {&sub_class($_)} @names ];
+    return $root;
 }
 
-sub unified_sub_class($){
-  my ($cls)=@_;
-  if (! exists $tree->{$cls}){
-    return &fuzzy_sub_class($cls);
-  } else {
-    return &sub_class($cls);
-  }
+sub unified_sub_class($) {
+    my ($cls) = @_;
+    if (!exists $tree->{$cls}) {
+        return &fuzzy_sub_class($cls);
+    }
+    else {
+        return &sub_class($cls);
+    }
 }
 
 #print Dumper(all_sub_classes);
-my $hierarchy=unified_sub_class $cls;
+my $hierarchy = unified_sub_class $cls;
 #print Dumper($hierarchy);
 #
-sub format_tree($;$){
-  my ($root,$verbose) = @_;
-  unless (%$root){
-    return ();
-  }
-  my $name = $root->{name};
-  my @child = @{$root->{child}};
+sub format_tree($;$) {
+    my ($root, $verbose) = @_;
+    unless (%$root) {
+        return ();
+    }
+    my $name = $root->{name};
+    my @child = @{$root->{child}};
 
-  my $line=$name;
-  if (defined($verbose) && exists $table->{$name}){
-    $line=$name. "\t[". $table->{$name}[0] . "]";
-  }
-  my @result=($line);
+    my $line = $name;
+    if (defined($verbose) && exists $table->{$name}) {
+        $line = $name . "\t[" . $table->{$name}[0] . "]";
+    }
+    my @result = ($line);
 
-  if (scalar(@child)==0){
+    if (scalar(@child) == 0) {
+        return @result;
+    }
+
+    my $last_child = pop @child;
+    foreach my $chd (@child) {
+        my ($first, @rest) = &format_tree($chd, $verbose);
+        push @result, "├── $first";
+        push @result, map {"│   $_"} @rest;
+    }
+    my ($first, @rest) = &format_tree($last_child, $verbose);
+    push @result, "└── $first";
+    push @result, map {"    $_"} @rest;
     return @result;
-  }
-
-  my $last_child = pop @child;
-  foreach my $chd (@child) {
-    my ($first, @rest)=&format_tree($chd, $verbose);
-    push @result,  "├── $first";
-    push @result,  map {"│   $_"} @rest;
-  }
-  my ($first, @rest)=&format_tree($last_child, $verbose);
-  push @result, "└── $first";
-  push @result, map {"    $_"} @rest;
-  return @result;
 }
 
 my @lines = format_tree $hierarchy, $verbose;
-print join qq//, map{"$_\n"} @lines;
+print join qq//, map {"$_\n"} @lines;
--- create_table.pl
+++ create_table.pl
@@ -4,144 +4,150 @@ use warnings;
 use Data::Dumper;
 use List::Util qw(max sum);
 
-my @UTF8_TABLE=((1) x 128, (-1) x 64, (2) x 32, (3) x 16,(4) x 8, (-5) x 8);
+my @UTF8_TABLE = ((1) x 128, (-1) x 64, (2) x 32, (3) x 16, (4) x 8, (-5) x 8);
 
 sub validate_utf8($) {
-  my $s = shift;
-  my @bytes = map {unpack "W", $_} split //, $s;
-  my $char_size = 0;
-  my @char_index = ();
-  for (my $i=0; $i < scalar(@bytes);$i+=$char_size) {
-    $char_size = $UTF8_TABLE[$bytes[$i]];
-    if ($char_size<=0) {
-      die sprintf("Invalid start utf8 char: char_size=%d, i=%d; %s", $char_size, $i, join(",", map {sprintf "%#b",$_} @bytes[0 .. $i]));
+    my $s = shift;
+    my @bytes = map {unpack "W", $_} split //, $s;
+    my $char_size = 0;
+    my @char_index = ();
+    for (my $i = 0; $i < scalar(@bytes); $i += $char_size) {
+        $char_size = $UTF8_TABLE[$bytes[$i]];
+        if ($char_size <= 0) {
+            die sprintf("Invalid start utf8 char: char_size=%d, i=%d; %s", $char_size, $i, join(",", map {sprintf "%#b", $_} @bytes[0 .. $i]));
+        }
+        for (my $k = $i + 1; $k < $i + $char_size; ++$k) {
+            if ($UTF8_TABLE[$bytes[$k]] != -1) {
+                die sprintf("Invalid continuation utf8 char: char_size=%d, i=%d; %s", $char_size, $k, join(",", map {sprintf "%#b", $_} @bytes[0 .. $k]));
+            }
+        }
+        push @char_index, $i;
     }
-    for (my $k = $i+1; $k < $i+$char_size; ++$k) {
-      if ($UTF8_TABLE[$bytes[$k]] != -1) {
-        die sprintf("Invalid continuation utf8 char: char_size=%d, i=%d; %s", $char_size, $k, join(",", map {sprintf "%#b",$_} @bytes[0 .. $k]));
-      }
-    }
-    push @char_index, $i;
-  }
-  return (\@char_index, \@bytes);
+    return (\@char_index, \@bytes);
 }
 
 sub utf8_length($) {
-  my $s = shift;
-  my ($char_index) = validate_utf8($s);
-  return scalar(@$char_index);
+    my $s = shift;
+    my ($char_index) = validate_utf8($s);
+    return scalar(@$char_index);
 }
 
 sub utf8_substr($$;$) {
-  my ($s,$off,$len) = @_;
-  my ($char_index, $bytes) = validate_utf8($s);
-  my $char_num = scalar(@$char_index);
-  if ($off < 0) {
-    $off += $char_num;
-  }
-  my @sub_bytes = @{$bytes}[$char_index->[$off] .. scalar(@$bytes)-1];
-  if ($off + $len < $char_num) {
-    @sub_bytes = @{$bytes}[$char_index->[$off] .. $char_index->[$off+$len-1]];
-  }
-  return join "", map{pack "W",$_} @sub_bytes;
+    my ($s, $off, $len) = @_;
+    my ($char_index, $bytes) = validate_utf8($s);
+    my $char_num = scalar(@$char_index);
+    if ($off < 0) {
+        $off += $char_num;
+    }
+    my @sub_bytes = @{$bytes}[$char_index->[$off] .. scalar(@$bytes) - 1];
+    if ($off + $len < $char_num) {
+        @sub_bytes = @{$bytes}[$char_index->[$off] .. $char_index->[$off + $len - 1]];
+    }
+    return join "", map {pack "W", $_} @sub_bytes;
 }
 
-my @data=();
-while(<>){
-  if (/^\s*$/){
-    push @data, undef;
-  } else {
-    s/^\s*(\S.*\S|\S+)\s*$/$1/g;
-    push @data, [split /\s*,\s*/, $_];
-  }
+my @data = ();
+while (<>) {
+    if (/^\s*$/) {
+        push @data, undef;
+    }
+    else {
+        s/^\s*(\S.*\S|\S+)\s*$/$1/g;
+        push @data, [ split /\s*,\s*/, $_ ];
+    }
 }
 #print Dumper(\@data);
 
 my $column_num = max(map {scalar(@$_)} grep {defined($_)} @data);
 
-@data=map { 
-  if (defined($_)) {
-    my @row =(@$_, ("") x $column_num);
-    [ @row[0 .. ($column_num -1)]]
-  } else{
-    $_
-  } 
+@data = map {
+    if (defined($_)) {
+        my @row = (@$_, ("") x $column_num);
+        [ @row[0 .. ($column_num - 1)] ]
+    }
+    else {
+        $_
+    }
 } @data;
 
 #print Dumper(\@data);
 
-my @max_len = map{my $col=$_; max(map {utf8_length($_->[$col])} grep {defined($_)} @data)} 0 .. ($column_num-1);
+my @max_len = map {my $col = $_;
+    max(map {utf8_length($_->[$col])} grep {defined($_)} @data)} 0 .. ($column_num - 1);
 
 #print Dumper(\@max_len);
 @data = map {
-  my $row = $_; 
-  if (defined($row)){
-    [ map {utf8_substr(' '.$row->[$_].(' ' x ($max_len[$_]+1)), 0, $max_len[$_]+2 )}  0 .. ($column_num-1)]
-  } else {
-    undef
-  }} @data;
+    my $row = $_;
+    if (defined($row)) {
+        [ map {utf8_substr(' ' . $row->[$_] . (' ' x ($max_len[$_] + 1)), 0, $max_len[$_] + 2)} 0 .. ($column_num - 1) ]
+    }
+    else {
+        undef
+    }} @data;
 #print Dumper(\@data);
-my @row_length=sum(@max_len)+$column_num+1;
+my @row_length = sum(@max_len) + $column_num + 1;
 
-my $thin_hrule='+'.(join qq/+/, map{'-' x ($_+2)} @max_len).'+';
-my $thick_hrule='+'.(join qq/+/, map{'=' x ($_+2)} @max_len).'+';
+my $thin_hrule = '+' . (join qq/+/, map {'-' x ($_ + 2)} @max_len) . '+';
+my $thick_hrule = '+' . (join qq/+/, map {'=' x ($_ + 2)} @max_len) . '+';
 
 
-sub print_thin_hrule(){
-  print "$thin_hrule\n";
+sub print_thin_hrule() {
+    print "$thin_hrule\n";
 }
 
-sub print_thick_hrule(){
-  print "$thick_hrule\n";
+sub print_thick_hrule() {
+    print "$thick_hrule\n";
 }
 
-sub print_data($){
-  my $row = shift;
-  print "|".(join qq/|/, @$row). "|\n";
+sub print_data($) {
+    my $row = shift;
+    print "|" . (join qq/|/, @$row) . "|\n";
 }
 
 
 sub remove_leading_undef(\@) {
-  my @array=@{shift;};
+    my @array = @{shift;};
 
-  while (1) {
-    my $elem = shift @array;
-    if (defined($elem)){
-      unshift @array, $elem;
-      last;
+    while (1) {
+        my $elem = shift @array;
+        if (defined($elem)) {
+            unshift @array, $elem;
+            last;
+        }
     }
-  }
-  return @array;
+    return @array;
 }
 
 sub remove_trailing_undef(\@) {
-  my @array=@{shift;};
-  while (1) {
-    my $elem = pop @array;
-    if (defined($elem)){
-      push @array, $elem;
-      last;
+    my @array = @{shift;};
+    while (1) {
+        my $elem = pop @array;
+        if (defined($elem)) {
+            push @array, $elem;
+            last;
+        }
     }
-  }
-  return @array;
+    return @array;
 }
 
 sub squeeze_undef(\@) {
-  my @array=@{shift;};
-  my $k = 0;
-  my $i = 1;
-  for (; $i <= $#array; ++$i) {
-    if (defined($array[$i])) {
-      $array[++$k] = $array[$i];
-    } else {
-      if (defined($array[$k])) {
-        $array[++$k] = undef;
-      } else {
-        # do nothing
-      }
+    my @array = @{shift;};
+    my $k = 0;
+    my $i = 1;
+    for (; $i <= $#array; ++$i) {
+        if (defined($array[$i])) {
+            $array[++$k] = $array[$i];
+        }
+        else {
+            if (defined($array[$k])) {
+                $array[++$k] = undef;
+            }
+            else {
+                # do nothing
+            }
+        }
     }
-  }
-  return @array[0 .. $k];
+    return @array[0 .. $k];
 }
 
 @data = remove_leading_undef @data;
@@ -154,11 +160,12 @@ my ($hdr, @body) = @data;
 print_thick_hrule;
 print_data $hdr;
 print_thick_hrule;
-foreach (0 .. $#body){
-  if (defined($body[$_])) {
-    print_data $body[$_];
-  } else {
-    print_thin_hrule;
-  }
+foreach (0 .. $#body) {
+    if (defined($body[$_])) {
+        print_data $body[$_];
+    }
+    else {
+        print_thin_hrule;
+    }
 }
 print_thin_hrule;
