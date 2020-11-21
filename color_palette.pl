0084a131c4ceb013ecb930ace605f98714d94cec
satanson <ranpanf@gmail.com>
2020-11-21 16:24:05 +0800

    fixup

--- color_palette.pl
+++ color_palette.pl
@@ -7,31 +7,31 @@
 use strict;
 use warnings;
 
+my ($expect_fg, $expect_bg, $expect_ef) = @ARGV;
 
-my ($expect_fg, $expect_bg, $expect_ef)=@ARGV;
-
-my @fg=(31..37,90..97);
-my @bg=(31..47,100..106);
-my @ef=(0..8);
-my $count=0;
-for (1..@fg*@bg*@ef) {
+my @fg = (31 .. 37, 90 .. 97);
+my @bg = (31 .. 47, 100 .. 106);
+my @ef = (0 .. 8);
+my $count = 0;
+for (1 .. @fg * @bg * @ef) {
   #my $i=($_-1)/(@bg*@ef);
   #my $j=($_-1)%(@bg*@ef)/@ef;
   #my $k=($_-1)%@ef;
-  my $i=($_-1)/(@bg*@ef);
-  my $j=($_-1)%(@bg*@ef)/@ef;
-  my $k=($_-1)%@ef;
-  my $fg=$fg[$i];
-  my $bg=$bg[$j];
-  my $ef=$ef[$k];
+  my $i = ($_ - 1) / (@bg * @ef);
+  my $j = ($_ - 1) % (@bg * @ef) / @ef;
+  my $k = ($_ - 1) % @ef;
+  my $fg = $fg[$i];
+  my $bg = $bg[$j];
+  my $ef = $ef[$k];
 
-  if ((!defined($expect_fg) || $expect_fg == $fg) && 
+  if ((!defined($expect_fg) || $expect_fg == $fg) &&
     (!defined($expect_bg) || $expect_bg == $bg) &&
     (!defined($expect_ef) || $expect_ef == $ef)) {
     print "\e[${fg};${bg};${ef}m \\e[${fg};${bg};${ef}m\\e[m\e[m";
-    if ($count % 8==0){
+    if ($count % 8 == 0) {
       print "\n";
-    } else{
+    }
+    else {
       print " ";
     }
     $count += 1;
