14006a0d6350146737c65cc5f930b931cb642f18
satanson <ranpanf@gmail.com>
2021-03-13 04:53:03 +0800

    complete a draft function match

--- include/function_match.hh
+++ include/function_match.hh
@@ -11,6 +11,8 @@
 #pragma once
 #include "unordered_set"
 #include "gtest/gtest.h"
+#include <list>
+#include <memory>
 
 // Collections for partial order relation over T^2
 template <typename T> class PartialOrderRelation {
@@ -23,6 +25,9 @@ public:
 
   template <typename... Args> void Put(T const &a, Args &&...bs) {
     ((put_one(a, std::forward<Args>(bs))), ...);
+  }
+  template <typename... Args> void PutReverse(T const &b, Args &&...as) {
+    ((put_one(std::forward<Args>(as), b)), ...);
   }
 
 private:
@@ -49,26 +54,162 @@ private:
 
 template <typename T> class TermDict {
 public:
+  TermDict(PartialOrderRelation<T> const& por): partial_orders(por) {
+    term_array.template emplace_back(empty_term());
+    term_tree.self = 0;
+    term_tree.parent = &term_tree;
+  }
   using TermType = std::vector<T>;
+  using TermArray = std::vector<TermType>;
   template <typename... Args> void Put(Args &&...terms) {
     ((put_one(std::forward<Args>(terms))), ...);
   }
   TermType BestMatch(TermType const &term) {
-    if (exact_match(term)){
-      return term;
-    }
     return minimum_superior(term);
   }
   bool IsInvalid(TermType const &term) { return term.empty(); }
 
 private:
-  void put_one(TermType const &term) {
-    if (IsInvalid(term)) {
-      throw std::invalid_argument("Empty term is invalid");
+  enum POCompareResult {
+    GT,
+    LT,
+    EQ,
+    UNKNOWN,
+  };
+
+  struct TermTree {
+    TermTree() = default;
+    TermTree(TermTree &&) = delete;
+    TermTree(TermTree &) = delete;
+    TermTree &operator=(TermTree &&) const = delete;
+    TermTree &operator=(TermTree &) const = delete;
+    ~TermTree() {
+      auto it = children.begin();
+      auto end = children.end();
+      for (; it != end; ++it) {
+        delete *it;
+      }
     }
+    size_t self;
+    std::list<TermTree *> children;
+    TermTree *parent;
+  };
+
+  POCompareResult compare_term(TermType const &lhs, TermType const &rhs) {
+    if (IsInvalid(rhs)) {
+      throw std::invalid_argument("Invalid term");
+    }
+    if (IsInvalid(lhs)) {
+      return POCompareResult::GT;
+    }
+    auto n = std::min(lhs.size(), rhs.size());
+    bool is_eq = (lhs.size() == rhs.size());
+    bool is_gt = true;
+    bool is_lt = true;
+    for (auto i = 0; i < n; ++i) {
+      auto &a = lhs[i];
+      auto &b = rhs[i];
+      if (a == b){
+        continue;
+      }
+
+      is_eq = false;
+      if (partial_orders.Contains(a, b)) {
+        is_gt = false;
+      } else if (partial_orders.Contains(b, a)) {
+        is_lt = false;
+      } else {
+        return POCompareResult::UNKNOWN;
+      }
+    }
+    if (is_eq) {
+      return POCompareResult::EQ;
+    }
+    if (is_gt) {
+      return POCompareResult::GT;
+    }
+    if (is_lt) {
+      return POCompareResult::LT;
+    }
+    return POCompareResult::UNKNOWN;
   }
 
-  bool exact_match(TermType const &term) { return true; }
-  TermType minimum_superior(TermType const &term) { return empty_term(); }
+  POCompareResult lookup(TermTree *tree, TermType const &new_term,
+                         TermTree **node) {
+    if (IsInvalid(new_term)) {
+      throw std::invalid_argument("Empty term is invalid");
+    }
+    auto result = compare_term(term_array[tree->self], new_term);
+    switch (result) {
+    case UNKNOWN:
+    case LT:
+    case EQ:
+      *node = tree;
+      return result;
+    }
+    for (auto& subtree : tree->children) {
+      auto child_result = lookup(subtree, new_term, node);
+      if (child_result != UNKNOWN) {
+        return child_result;
+      }
+    }
+    *node = tree;
+    return GT;
+  }
+
+  void put_one(TermType const &term) {
+    if (IsInvalid(term)) {
+      throw std::invalid_argument("Invalid term");
+    }
+    TermTree *node;
+    auto result = lookup(&term_tree, term, &node);
+    if (result == EQ) {
+      throw std::invalid_argument("Duplicate term");
+    }
+    if (result == UNKNOWN) {
+      throw std::invalid_argument("Impossible error");
+    }
+    size_t term_idx = term_array.size();
+    term_array.push_back(term);
+    auto new_node = new TermTree();
+    new_node->self = term_idx;
+    if (result == LT) {
+      // add node into new_node as children
+      new_node->children.push_back(node);
+      // set new_node's parent to node's parent
+      new_node->parent = node->parent;
+      // set node's parent to new node
+      node->parent = new_node;
+
+      auto child_it = new_node->parent->children.begin();
+      auto child_end = new_node->parent->children.end();
+      for (; child_it != child_end; ++child_it) {
+        if ((*child_it)->self == node->self) {
+          *child_it = new_node;
+          break;
+        }
+      }
+    } else {
+      new_node->parent = node;
+      node->children.push_back(new_node);
+    }
+  }
+  TermType minimum_superior(TermType const &term) {
+    TermTree *node;
+    auto result = lookup(&term_tree, term, &node);
+    if (result == EQ) {
+      return term_array[node->self];
+    }
+    if (result == LT) {
+      return term_array[node->parent->self];
+    }
+    if (result == GT) {
+      return term_array[node->self];
+    }
+    return empty_term();
+  }
   TermType empty_term() { return TermType(); }
+  TermTree term_tree;
+  TermArray term_array;
+  PartialOrderRelation<T> partial_orders;
 };
--- unittest/test_function_match.cc
+++ unittest/test_function_match.cc
@@ -25,8 +25,86 @@ TEST_F(FunctionMatchTest, test_partial_order_relation) {
 TEST_F(FunctionMatchTest, test_partial_order_relation_fail) {
   PartialOrderRelation<int> por;
   por.Put(1, 2, 3, 4);
-  ASSERT_ANY_THROW(por.Put(1,2));
-  ASSERT_ANY_THROW(por.Put(4,1));
+  ASSERT_ANY_THROW(por.Put(1, 2));
+  ASSERT_ANY_THROW(por.Put(4, 1));
+}
+enum DataType {
+  TYPE_INT,
+  TYPE_BIGINT,
+  TYPE_FLOAT,
+  TYPE_DOUBLE,
+  TYPE_STRING,
+  TYPE_DECIMAL32,
+  TYPE_DECIMAL64,
+  TYPE_DECIMAL128,
+};
+std::string datatype_to_string(DataType dt) {
+  switch (dt) {
+  case TYPE_INT:
+    return "int";
+  case TYPE_BIGINT:
+    return "bigint";
+  case TYPE_FLOAT:
+    return "float";
+  case TYPE_DOUBLE:
+    return "double";
+  case TYPE_STRING:
+    return "string";
+  case TYPE_DECIMAL32:
+    return "decimal32";
+  case TYPE_DECIMAL64:
+    return "decimal64";
+  case TYPE_DECIMAL128:
+    return "decimal128";
+  }
+  return "unknown";
+}
+
+std::string term_to_string(std::vector<DataType> const &term) {
+  if (term.empty()) {
+    return "term()";
+  }
+  if (term.size() == 1) {
+    return std::string("term(") + datatype_to_string(term[0]) + ")";
+  }
+  std::stringstream ss;
+  ss << "term(" << datatype_to_string(term.front());
+  for (auto i = 1; i < term.size(); ++i) {
+    ss << ", " << datatype_to_string(term[i]);
+  }
+  ss << ")";
+  return ss.str();
+}
+
+TEST_F(FunctionMatchTest, test_function_match_ok) {
+  PartialOrderRelation<DataType> por;
+  por.PutReverse(TYPE_DOUBLE, TYPE_INT, TYPE_BIGINT, TYPE_FLOAT, TYPE_DECIMAL32,
+                 TYPE_DECIMAL64, TYPE_DECIMAL128);
+  por.PutReverse(TYPE_BIGINT, TYPE_INT);
+  por.PutReverse(TYPE_DECIMAL64, TYPE_DECIMAL32);
+  por.PutReverse(TYPE_DECIMAL128, TYPE_DECIMAL32, TYPE_DECIMAL64);
+  por.PutReverse(TYPE_STRING, TYPE_INT, TYPE_BIGINT, TYPE_FLOAT, TYPE_DOUBLE,
+                 TYPE_DECIMAL32, TYPE_DECIMAL64, TYPE_DECIMAL128);
+  TermDict<DataType> td(por);
+  using TermType = TermDict<DataType>::TermType;
+  td.Put(TermType{TYPE_INT, TYPE_INT, TYPE_INT});
+
+  td.Put(TermType{TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE});
+  td.Put(TermType{TYPE_FLOAT, TYPE_BIGINT, TYPE_DOUBLE});
+  td.Put(TermType{TYPE_DECIMAL128, TYPE_DECIMAL128, TYPE_DECIMAL128});
+  td.Put(TermType{TYPE_STRING, TYPE_STRING, TYPE_STRING});
+  auto a = td.BestMatch(TermType{TYPE_INT, TYPE_INT, TYPE_INT});
+  std::cout << term_to_string(a) << std::endl;
+  auto b = td.BestMatch(TermType{TYPE_BIGINT, TYPE_INT, TYPE_INT});
+  std::cout << term_to_string(b) << std::endl;
+  auto c = td.BestMatch(TermType{TYPE_DECIMAL32, TYPE_INT, TYPE_INT});
+  std::cout << term_to_string(c) << std::endl;
+  auto d =
+      td.BestMatch(TermType{TYPE_DECIMAL32, TYPE_DECIMAL32, TYPE_DECIMAL32});
+  std::cout << term_to_string(d) << std::endl;
+  auto e =
+      td.BestMatch(TermType{TYPE_DECIMAL64, TYPE_DECIMAL64, TYPE_DECIMAL64});
+  std::cout << term_to_string(e) << std::endl;
 }
 
 } // namespace test
