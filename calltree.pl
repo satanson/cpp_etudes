c6a3db7f1b7ab350022fd49e14cede06eac88f94
satanson <ranpanf@gmail.com>
2022-12-11 00:20:08 +0800

    format calltree.pl

--- calltree.pl
+++ calltree.pl
@@ -87,9 +87,9 @@ sub get_path_of_script() {
 sub mind(@) {
   my ($title, @lines) = @_;
   # [level, content, children]
-  my @stk=([0, $title, []]);
+  my @stk = ([ 0, $title, [] ]);
   foreach my $ln (@lines) {
-    if ($ln =~/^([-]*)\s*(.*)/) {
+    if ($ln =~ /^([-]*)\s*(.*)/) {
       my ($dashes, $content) = ($1, $2);
       my $level = length($dashes);
       while (scalar(@stk) > 0 && $stk[-1][0] + 1 != $level) {
@@ -97,9 +97,9 @@ sub mind(@) {
       }
       die "Ill-formed mind input" if (scalar(@stk) == 0);
       my $top = $stk[-1];
-      my $curr = [$level, $content, []];
+      my $curr = [ $level, $content, [] ];
       push @stk, $curr;
-      my ($top_level,$parent_content,$top_children) = @$top;
+      my ($top_level, $parent_content, $top_children) = @$top;
       push @$top_children, $curr;
     }
   }
@@ -1523,7 +1523,7 @@ sub format_tree($$$$\&\&) {
 
   foreach my $chd (@child) {
     my ($first, @rest) = &format_tree($chd, $level + 1, $verbose, $enable_prune, $get_entry, $get_child);
-    if ((!defined($first) || $first =~/^\s*$/) && scalar(@rest)==0) {
+    if ((!defined($first) || $first =~ /^\s*$/) && scalar(@rest) == 0) {
       push @result, "│";
       next;
     }
@@ -1638,32 +1638,33 @@ sub format_mind($) {
   };
   my $get_child = sub($) {
     my $root = shift;
-    return map {+{level=>$_->[0], name=>$_->[1], child=>$_->[2]}} @{$root->{child}};
+    return map {+{ level => $_->[0], name => $_->[1], child => $_->[2] }} @{$root->{child}};
   };
   my $tree_root = {
-    level=>$root->[0],
-    name=>$root->[1],
-    child=>$root->[2],
+    level => $root->[0],
+    name  => $root->[1],
+    child => $root->[2],
   };
 
   &format_tree($tree_root, 0, 0, 0, $get_entry, $get_child);
 }
 
-sub help(){
-  my %subst_tab=(
-    "<cs1>"=>"\e[33;93;3m",
-    "<cs2>"=>"\e[31;32;1m",
-    "<cs3>"=>"\e[31;31;1m",
-    "<end>"=>"\e[m",
+sub help() {
+  my %subst_tab = (
+    "<cs1>" => "\e[33;93;3m",
+    "<cs2>" => "\e[31;32;1m",
+    "<cs3>" => "\e[31;31;1m",
+    "<end>" => "\e[m",
   );
-  my @lines = map{
-    my $ln=$_; 
-    foreach my $pat (keys %subst_tab){
-      $ln=~s/$pat/$subst_tab{$pat}/g;
+  my @lines = map {
+    my $ln = $_;
+    foreach my $pat (keys %subst_tab) {
+      $ln =~ s/$pat/$subst_tab{$pat}/g;
     }
     $ln;
-  } map{chop;$_} <DATA>;
-  print join qq//, map{"$_\n"} format_mind(mind(@lines));
+  } map {chop;
+    $_} <DATA>;
+  print join qq//, map {"$_\n"} format_mind(mind(@lines));
 }
 
 sub get_entry_of_called_tree($$$) {
@@ -1886,8 +1887,14 @@ sub cached_sha256_file(@) {
 
 my @key = (@ARGV, $Global_isatty, $env_trivial_threshold, $env_length_threshold);
 
-my $Opt_func = shift || do{ help(); exit(1);};
-do {help(); exit(1);} if ("\U$Opt_func\E"=~/^[-]*H(E(L(P)?)?)?$/ && scalar(@ARGV)==0);
+my $Opt_func = shift || do {
+  help();
+  exit(1);
+};
+do {
+  help();
+  exit(1);
+} if ("\U$Opt_func\E" =~ /^[-]*H(E(L(P)?)?)?$/ && scalar(@ARGV) == 0);
 my $Opt_func_match_rule = shift;
 my $Opt_mode = shift;
 my $Opt_verbose = shift;
---
+++ data.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+
+print join"\n",<DATA>;
+
+
+__DATA__
+`dafaf`
+dafsfa
+
+dasfsa
+f
+
+__END__
