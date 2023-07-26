7fba6fa1d417ba92598e1376744fcfcc9a8b9ef9
satanson <ranpanf@gmail.com>
2023-07-27 00:33:30 +0800

    my first successful jit example from toy4

---
+++ include/llvm.hh
@@ -0,0 +1,167 @@
+//
+// Created by grakra on 23-7-26.
+//
+
+#pragma once
+#include <absl/status/statusor.h>
+#include <llvm-c/BitWriter.h>
+#include <llvm-c/Core.h>
+#include <llvm-c/LLJIT.h>
+#include <llvm-c/Orc.h>
+#include <llvm-c/OrcEE.h>
+#include <llvm-c/TargetMachine.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/ExecutionEngine/Orc/ExecutorProcessControl.h>
+#include <llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h>
+#include <llvm/ExecutionEngine/Orc/LLJIT.h>
+#include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/Support/Error.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Transforms/InstCombine/InstCombine.h>
+#include <llvm/Transforms/Scalar.h>
+#include <llvm/Transforms/Scalar/GVN.h>
+
+using llvm::orc::SelfExecutorProcessControl;
+using llvm::orc::ExecutionSession;
+using llvm::orc::JITTargetMachineBuilder;
+using llvm::DataLayout;
+using llvm::orc::MangleAndInterner;
+using llvm::orc::IRCompileLayer;
+using llvm::orc::RTDyldObjectLinkingLayer;
+using llvm::SectionMemoryManager;
+using llvm::orc::JITDylib;
+using llvm::orc::DynamicLibrarySearchGenerator;
+using llvm::orc::ConcurrentIRCompiler;
+using llvm::LLVMContext;
+using llvm::Module;
+using absl::StatusOr;
+using absl::Status;
+struct LLVMInitialization {
+    LLVMInitialization() {
+        llvm::InitializeNativeTarget();
+        llvm::InitializeNativeTargetAsmPrinter();
+        llvm::InitializeNativeTargetAsmParser();
+    }
+};
+class EtudesJIT;
+using EtudesJITPtr = std::unique_ptr<EtudesJIT>;
+class EtudesJIT {
+private:
+    static Status llvm_error_to_status_error(const llvm::Error& error) {
+        std::string s;
+        llvm::raw_string_ostream rso(s);
+        rso << error;
+        return Status(absl::StatusCode::kInternal, rso.str());
+    }
+
+    std::unique_ptr<ExecutionSession> _es;
+    JITTargetMachineBuilder _jit_tmb;
+    llvm::DataLayout _data_layout;
+    MangleAndInterner _mangling;
+    std::unique_ptr<RTDyldObjectLinkingLayer> _object_layer;
+    std::unique_ptr<IRCompileLayer> _compiler_layer;
+    JITDylib& _main_jd;
+    std::unique_ptr<LLVMContext> _context;
+    std::unique_ptr<Module> _module;
+
+    EtudesJIT(const EtudesJIT&) = delete;
+
+public:
+    EtudesJIT(std::unique_ptr<ExecutionSession>&& es, JITTargetMachineBuilder&& jit_tmb, llvm::DataLayout&& data_layout,
+              std::unique_ptr<RTDyldObjectLinkingLayer>&& object_layer, std::unique_ptr<IRCompileLayer>&& compile_layer,
+              JITDylib& main_jd)
+            : _es(std::move(es)),
+              _jit_tmb(std::move(jit_tmb)),
+              _data_layout(std::move(data_layout)),
+              _mangling(*_es, _data_layout),
+              _object_layer(std::move(object_layer)),
+              _compiler_layer(std::move(compile_layer)),
+              _main_jd(main_jd),
+              _context(std::make_unique<LLVMContext>()),
+              _module(std::make_unique<Module>("jit", *_context)) {
+        _module->setDataLayout(_data_layout);
+        _module->setTargetTriple(_jit_tmb.getTargetTriple().getTriple());
+    }
+
+    ~EtudesJIT() { llvm::cantFail(_es->endSession()); }
+
+    static StatusOr<EtudesJITPtr> create() {
+        auto epc = SelfExecutorProcessControl::Create();
+        if (!epc) {
+            return llvm_error_to_status_error(epc.takeError());
+        }
+        auto es = std::make_unique<ExecutionSession>(std::move(*epc));
+
+        JITTargetMachineBuilder jit_tmb(es->getExecutorProcessControl().getTargetTriple());
+        auto data_layout = jit_tmb.getDefaultDataLayoutForTarget();
+        if (!data_layout) {
+            return llvm_error_to_status_error(data_layout.takeError());
+        }
+
+        std::unique_ptr<RTDyldObjectLinkingLayer> object_layer = std::make_unique<RTDyldObjectLinkingLayer>(
+                *es, []() { return std::make_unique<SectionMemoryManager>(); });
+
+        std::unique_ptr<IRCompileLayer> compiler_layer =
+                std::make_unique<IRCompileLayer>(*es, *object_layer, std::make_unique<ConcurrentIRCompiler>(jit_tmb));
+        JITDylib& main_jd = es->createBareJITDylib("<main>");
+        auto search_generator = DynamicLibrarySearchGenerator::GetForCurrentProcess(data_layout->getGlobalPrefix());
+        if (!search_generator) {
+            return llvm_error_to_status_error(search_generator.takeError());
+        }
+        main_jd.addGenerator(std::move(*search_generator));
+        if (jit_tmb.getTargetTriple().isOSBinFormatCOFF()) {
+            object_layer->setOverrideObjectFlagsWithResponsibilityFlags(true);
+            object_layer->setAutoClaimResponsibilityForObjectSymbols(true);
+        }
+
+        return std::make_unique<EtudesJIT>(std::move(es), std::move(jit_tmb), std::move(*data_layout),
+                                           std::move(object_layer), std::move(compiler_layer), main_jd);
+    }
+    void opt(bool debug) {
+        auto fn_pass_mgr = std::make_unique<llvm::legacy::FunctionPassManager>(_module.get());
+
+        // Do simple "peephole" optimizations and bit-twiddling optzns.
+        fn_pass_mgr->add(llvm::createInstructionCombiningPass());
+        // Reassociate expressions.
+        fn_pass_mgr->add(llvm::createReassociatePass());
+        // Eliminate Common SubExpressions.
+        fn_pass_mgr->add(llvm::createGVNPass());
+        // Simplify the control flow graph (deleting unreachable blocks, etc).
+        fn_pass_mgr->add(llvm::createCFGSimplificationPass());
+        fn_pass_mgr->doInitialization();
+        if (debug) {
+            llvm::outs() << "Before optimization:\n";
+            _module->print(llvm::outs(), nullptr);
+        }
+        for (auto& fn : _module->getFunctionList()) {
+            fn_pass_mgr->run(fn);
+        }
+        if (debug) {
+            llvm::outs() << "After optimization:\n";
+            _module->print(llvm::outs(), nullptr);
+        }
+    }
+
+    std::unique_ptr<LLVMContext>& get_llvm_context() { return _context; }
+    std::unique_ptr<Module>& get_module() { return _module; };
+    std::unique_ptr<llvm::orc::ThreadSafeModule> _ts_module;
+    uintptr_t look_up(const std::string& fn_name) {
+        auto res_tracker = _main_jd.getDefaultResourceTracker();
+        if (!res_tracker) {
+            res_tracker = _main_jd.createResourceTracker();
+        }
+        DCHECK(res_tracker);
+        if (!_ts_module) {
+            auto ts_module = llvm::orc::ThreadSafeModule(std::move(_module), std::move(_context));
+            cantFail(_compiler_layer->add(res_tracker, std::move(ts_module)));
+        }
+
+        auto symbol = cantFail(_es->lookup({&_main_jd}, _mangling(fn_name)));
+        // Get the symbol's address and cast it to the right type (takes no
+        // arguments, returns a double) so we can call it as a native function.
+        return reinterpret_cast<uintptr_t>(symbol.getAddress());
+    }
+};
--- unittest/test_llvm.cc
+++ unittest/test_llvm.cc
@@ -11,13 +11,27 @@
 #include <llvm-c/BitWriter.h>
 #include <llvm-c/Core.h>
 #include <llvm-c/LLJIT.h>
-#include <llvm-c/OrcEE.h>
 #include <llvm-c/Orc.h>
+#include <llvm-c/OrcEE.h>
 #include <llvm-c/TargetMachine.h>
-#include <llvm/ExecutionEngine/Orc/LLJIT.h>
+#include <llvm/ADT/StringRef.h>
 #include <llvm/ExecutionEngine/Orc/ExecutorProcessControl.h>
+#include <llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h>
+#include <llvm/ExecutionEngine/Orc/LLJIT.h>
+#include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/Support/Error.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Transforms/InstCombine/InstCombine.h>
+#include <llvm/Transforms/Scalar.h>
+#include <llvm/Transforms/Scalar/GVN.h>
 
+#include <llvm.hh>
 #include <memory>
+using namespace llvm::orc;
+using namespace llvm;
 namespace test {
 class LLVMTest : public ::testing::Test {};
 typedef struct OpaqueObj* ObjRef;
@@ -109,15 +123,12 @@ static void llvm_log_jit_error(void* ctx, LLVMErrorRef error) {
 }
 
 static void materializationUnitFn() {}
-static LLVMErrorRef
-definitionGeneratorFn(LLVMOrcDefinitionGeneratorRef G, void *Ctx,
-                      LLVMOrcLookupStateRef *LS, LLVMOrcLookupKind K,
-                      LLVMOrcJITDylibRef JD, LLVMOrcJITDylibLookupFlags F,
-                      LLVMOrcCLookupSet Names, size_t NamesCount) {
+static LLVMErrorRef definitionGeneratorFn(LLVMOrcDefinitionGeneratorRef G, void* Ctx, LLVMOrcLookupStateRef* LS,
+                                          LLVMOrcLookupKind K, LLVMOrcJITDylibRef JD, LLVMOrcJITDylibLookupFlags F,
+                                          LLVMOrcCLookupSet Names, size_t NamesCount) {
     for (size_t I = 0; I < NamesCount; I++) {
         LLVMOrcCLookupSetElement Element = Names[I];
-        LLVMOrcJITTargetAddress Addr =
-                (LLVMOrcJITTargetAddress)(&materializationUnitFn);
+        LLVMOrcJITTargetAddress Addr = (LLVMOrcJITTargetAddress)(&materializationUnitFn);
         LLVMJITSymbolFlags Flags = {LLVMJITSymbolGenericFlagsWeak, 0};
         LLVMJITEvaluatedSymbol Sym = {Addr, Flags};
         LLVMOrcRetainSymbolStringPoolEntry(Element.Name);
@@ -125,8 +136,7 @@ definitionGeneratorFn(LLVMOrcDefinitionGeneratorRef G, void *Ctx,
         LLVMJITCSymbolMapPair Pairs[] = {Pair};
         LLVMOrcMaterializationUnitRef MU = LLVMOrcAbsoluteSymbols(Pairs, 1);
         LLVMErrorRef Err = LLVMOrcJITDylibDefine(JD, MU);
-        if (Err)
-            return Err;
+        if (Err) return Err;
     }
     return LLVMErrorSuccess;
 }
@@ -173,7 +183,6 @@ TEST_F(LLVMTest, testBasic2) {
     std::cout << LLVMPrintModuleToString(module) << std::endl;
     auto orcTsCtx = LLVMOrcCreateNewThreadSafeContext();
 
-
     LLVMOrcLLJITRef lljit;
     auto orcJitBuilder = LLVMOrcCreateLLJITBuilder();
     auto orcJitTargetMachineBuilder = LLVMOrcJITTargetMachineBuilderCreateFromTargetMachine(targetMachine);
@@ -214,96 +223,118 @@ TEST_F(LLVMTest, testBasic2) {
 }
 
 TEST_F(LLVMTest, testBasic3) {
-    auto llvmCtx = LLVMContextCreate();
-    ASSERT_TRUE(LLVMInitializeNativeTarget() == 0);
-    auto module = LLVMModuleCreateWithName("m0");
-    std::string triple = LLVMNormalizeTargetTriple(LLVMGetDefaultTargetTriple());
-    auto target = LLVMGetFirstTarget();
-    ASSERT_TRUE(target != nullptr);
-    std::string hostCPU = LLVMGetHostCPUName();
-    std::string hostCPUFeatures = LLVMGetHostCPUFeatures();
-    auto targetMachine = LLVMCreateTargetMachine(target, triple.c_str(), hostCPU.c_str(), hostCPUFeatures.c_str(),
-                                                 LLVMCodeGenOptLevel::LLVMCodeGenLevelDefault,
-                                                 LLVMRelocMode::LLVMRelocPIC, LLVMCodeModel::LLVMCodeModelDefault);
-    auto targetDataLayout = LLVMCreateTargetDataLayout(targetMachine);
-    auto dataLayout = LLVMCopyStringRepOfTargetData(targetDataLayout);
-    LLVMSetTarget(module, triple.c_str());
-    LLVMSetDataLayout(module, dataLayout);
-    auto builder = LLVMCreateBuilder();
-    std::vector<LLVMTypeRef> funcParams(2, LLVMInt32Type());
-    auto funcType = LLVMFunctionType(LLVMInt32Type(), funcParams.data(), funcParams.size(), false);
-    auto func = LLVMAddFunction(module, "foobar", funcType);
-    LLVMSetLinkage(func, LLVMExternalLinkage);
-    LLVMSetVisibility(func, LLVMDefaultVisibility);
-    auto block0 = LLVMAppendBasicBlock(func, "entry");
-    auto param0 = LLVMGetParam(func, 0);
-    auto param1 = LLVMGetParam(func, 1);
-    LLVMPositionBuilderAtEnd(builder, block0);
-    auto tmp = LLVMBuildAlloca(builder, LLVMInt32Type(), "alloc");
-    auto addResult = LLVMBuildAdd(builder, param0, param1, "add");
-    LLVMBuildStore(builder, addResult, tmp);
-    auto c = LLVMBuildLoad2(builder, LLVMInt32Type(), tmp, "");
-    auto d = LLVMConstInt(LLVMInt32Type(), 2, 0);
-    auto e = LLVMBuildMul(builder, c, d, "");
-    LLVMBuildRet(builder, e);
-    LLVMDisposeBuilder(builder);
-    //auto buffer = LLVMWriteBitcodeToMemoryBuffer(module);
-    //const auto* start = LLVMGetBufferStart(buffer);
-    //const auto size = LLVMGetBufferSize(buffer);
-    //std::string_view s(start, size);
-    //std::cout<<s<<std::endl;
-    std::cout << LLVMPrintModuleToString(module) << std::endl;
-    auto orcTsCtx = LLVMOrcCreateNewThreadSafeContext();
-
+    InitializeNativeTarget();
+    InitializeNativeTargetAsmPrinter();
+    InitializeNativeTargetAsmParser();
     auto EPC = llvm::orc::SelfExecutorProcessControl::Create();
     if (!EPC) {
-        std::cout<<toString(EPC.takeError())<<std::endl;
+        errs() << EPC.takeError();
         return;
     }
-    /*
     auto ES = std::make_unique<llvm::orc::ExecutionSession>(std::move(*EPC));
-    JITTargetMachineBuilder JTMB(
-            ES->getExecutorProcessControl().getTargetTriple());
+    llvm::orc::JITTargetMachineBuilder JTMB(ES->getExecutorProcessControl().getTargetTriple());
+    auto DL = cantFail(JTMB.getDefaultDataLayoutForTarget());
+    using llvm::orc::MangleAndInterner;
+    using llvm::orc::IRCompileLayer;
+    MangleAndInterner Mangle(*ES, DL);
+    RTDyldObjectLinkingLayer ObjectLayer(*ES, []() { return std::make_unique<SectionMemoryManager>(); });
+    IRCompileLayer CompileLayer(*ES, ObjectLayer, std::make_unique<ConcurrentIRCompiler>(JTMB));
+    JITDylib& MainJD = ES->createBareJITDylib("<main>");
+    MainJD.addGenerator(cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(DL.getGlobalPrefix())));
 
-
-    LLVMOrcLLJITRef lljit;
-    auto orcJitBuilder = LLVMOrcCreateLLJITBuilder();
-    auto orcJitTargetMachineBuilder = LLVMOrcJITTargetMachineBuilderCreateFromTargetMachine(targetMachine);
-    LLVMOrcLLJITBuilderSetJITTargetMachineBuilder(orcJitBuilder, orcJitTargetMachineBuilder);
-    LLVMOrcLLJITBuilderSetObjectLinkingLayerCreator(orcJitBuilder, llvm_create_object_layer, NULL);
-    auto err = LLVMOrcCreateLLJIT(&lljit, orcJitBuilder);
-    if (err) {
-        LOG(ERROR) << "error: " << LLVMGetErrorMessage(err);
+    if (JTMB.getTargetTriple().isOSBinFormatCOFF()) {
+        ObjectLayer.setOverrideObjectFlagsWithResponsibilityFlags(true);
+        ObjectLayer.setAutoClaimResponsibilityForObjectSymbols(true);
     }
 
-    LLVMOrcExecutionSessionSetErrorReporter(LLVMOrcLLJITGetExecutionSession(lljit), llvm_log_jit_error, NULL);
+    auto TheContext = std::make_unique<LLVMContext>();
+    auto TheModule = std::make_unique<Module>("my cool jit", *TheContext);
+    TheModule->setDataLayout(DL);
+    TheModule->setTargetTriple(JTMB.getTargetTriple().getTriple());
+    // Create a new builder for the module.
+    auto Builder = std::make_unique<IRBuilder<>>(*TheContext);
+    std::vector<llvm::Type*> funcParamTypes(2, llvm::Type::getInt32Ty(*TheContext));
+    auto funcType = llvm::FunctionType::get(llvm::Type::getInt32Ty(*TheContext), funcParamTypes, false);
+    auto func = Function::Create(funcType, llvm::GlobalValue::ExternalLinkage, "foobar", TheModule.get());
+    auto block0 = BasicBlock::Create(*TheContext, "entry", func, nullptr);
+    auto arg0 = func->getArg(0);
+    auto arg1 = func->getArg(1);
+    Builder->SetInsertPoint(block0);
+    auto tmp = Builder->CreateAlloca(llvm::Type::getInt32Ty(*TheContext), nullptr, "alloc");
+    auto addResult = Builder->CreateAdd(arg0, arg1, "add");
+    Builder->CreateStore(addResult, tmp);
+    auto c = Builder->CreateLoad(llvm::Type::getInt32Ty(*TheContext), tmp);
+    auto d = ConstantInt::get(llvm::Type::getInt32Ty(*TheContext), 2, true);
+    auto e = Builder->CreateMul(c, d, "mul");
+    Builder->CreateRet(e);
 
-    LLVMOrcDefinitionGeneratorRef main_gen, ref_gen;
-    err = LLVMOrcCreateDynamicLibrarySearchGeneratorForProcess(&main_gen, LLVMOrcLLJITGetGlobalPrefix(lljit), 0, NULL);
-    if (err) {
-        std::cout << "error" << LLVMGetErrorMessage(err) << std::endl;
+    // Create a new pass manager attached to it.
+    auto TheFPM = std::make_unique<legacy::FunctionPassManager>(TheModule.get());
+
+    // Do simple "peephole" optimizations and bit-twiddling optzns.
+    TheFPM->add(createInstructionCombiningPass());
+    // Reassociate expressions.
+    TheFPM->add(createReassociatePass());
+    // Eliminate Common SubExpressions.
+    TheFPM->add(createGVNPass());
+    // Simplify the control flow graph (deleting unreachable blocks, etc).
+    TheFPM->add(createCFGSimplificationPass());
+    TheFPM->doInitialization();
+    TheModule->print(outs(), nullptr);
+    TheFPM->run(*func);
+    TheModule->print(outs(), nullptr);
+
+    auto RT = MainJD.createResourceTracker();
+
+    auto TSM = ThreadSafeModule(std::move(TheModule), std::move(TheContext));
+    RT = MainJD.getDefaultResourceTracker();
+    cantFail(CompileLayer.add(RT, std::move(TSM)));
+
+    StringRef Name = "foobar";
+    auto ExprSymbol = cantFail(ES->lookup({&MainJD}, Mangle(Name.str())));
+
+    // Get the symbol's address and cast it to the right type (takes no
+    // arguments, returns a double) so we can call it as a native function.
+    auto f = (int (*)(int, int))(intptr_t)ExprSymbol.getAddress();
+    std::cout << "result=" << f(100, 20) << std::endl;
+
+    // Delete the anonymous expression module from the JIT.
+    ExitOnError ExitOnError;
+    ExitOnError(RT->remove());
+    if (auto Err = ES->endSession()) {
+        ES->reportError(std::move(Err));
     }
-    LLVMOrcJITDylibAddGenerator(LLVMOrcLLJITGetMainJITDylib(lljit), main_gen);
+}
 
-    ref_gen = LLVMOrcCreateCustomCAPIDefinitionGenerator(definitionGeneratorFn, NULL);
-    LLVMOrcJITDylibAddGenerator(LLVMOrcLLJITGetMainJITDylib(lljit), ref_gen);
+TEST_F(LLVMTest, testBasic4) {
+    LLVMInitialization();
+    auto jit_result = EtudesJIT::create();
+    ASSERT_TRUE(jit_result.ok());
+    auto jit = std::move(jit_result.value());
 
-    auto orcTsModule = LLVMOrcCreateNewThreadSafeModule(module, orcTsCtx);
-    LLVMOrcJITDylibRef jd = LLVMOrcLLJITGetMainJITDylib(lljit);
-    auto rt = LLVMOrcJITDylibCreateResourceTracker(jd);
+    auto& context = jit->get_llvm_context();
+    auto& module = jit->get_module();
+    auto Builder = std::make_unique<IRBuilder<>>(*context);
+    std::vector<llvm::Type*> funcParamTypes(2, llvm::Type::getInt32Ty(*context));
+    auto funcType = llvm::FunctionType::get(llvm::Type::getInt32Ty(*context), funcParamTypes, false);
+    auto func = Function::Create(funcType, llvm::GlobalValue::ExternalLinkage, "foobar", module.get());
+    auto block0 = BasicBlock::Create(*context, "entry", func, nullptr);
+    auto arg0 = func->getArg(0);
+    auto arg1 = func->getArg(1);
+    Builder->SetInsertPoint(block0);
+    auto tmp = Builder->CreateAlloca(llvm::Type::getInt32Ty(*context), nullptr, "alloc");
+    auto addResult = Builder->CreateAdd(arg0, arg1, "add");
+    Builder->CreateStore(addResult, tmp);
+    auto c = Builder->CreateLoad(llvm::Type::getInt32Ty(*context), tmp);
+    auto d = ConstantInt::get(llvm::Type::getInt32Ty(*context), 2, true);
+    auto e = Builder->CreateMul(c, d, "mul");
+    Builder->CreateRet(e);
 
-    err = LLVMOrcLLJITAddLLVMIRModuleWithRT(lljit, rt, orcTsModule);
-    if (err) {
-        LOG(ERROR) << "error: " << LLVMGetErrorMessage(err);
-    }
-    LLVMOrcExecutorAddress addr;
-    err = LLVMOrcLLJITLookup(lljit, &addr, "foobar");
-    if (err) {
-        LOG(ERROR) << "error: " << LLVMGetErrorMessage(err);
-    }
-    auto f = reinterpret_cast<int (*)(int, int)>(reinterpret_cast<uintptr_t>(addr));
-    std::cout << f(10, 100) << std::endl;
-     */
+    jit->opt(true);
+    auto addr = jit->look_up("foobar");
+
+    auto f = (int (*)(int, int))addr;
+    std::cout << "result=" << f(100, 20) << std::endl;
 }
 
 } // namespace test
